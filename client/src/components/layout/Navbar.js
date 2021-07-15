c58c6a28864803569f95a5870852d4eaee684946
Daniel <turtleluck20@gmail.com>
2021-07-16 01:29:23 +1000

    [Update]: fix some style issue on navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,31 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import NavbarImg from "../image/navbar.svg"
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
-        {/* <nav className="z-depth-0"> */}
-          {/* <div className="nav-wrapper white"> */}
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-            <p style={{color: 'white', fontSize: '53px', textAlign: 'right', paddingRight: '3%'}}><span style={{color:'#FF9100'}}>P</span>ERMIERE <span style={{color:'#FF9100'}}>F</span>IVERR <span style={{color:'#FF9100'}}>E</span>NVATO</p>
-            </Link>
-        {/* </nav> */}
+      <div className='navbar-fixed'>
+        <Link
+          to='/'
+          style={{
+            fontFamily: 'monospace'
+          }}
+          className='col s5 brand-logo center black-text'>
+          <p
+            style={{
+              color: 'white',
+              fontSize: '53px',
+              textAlign: 'right',
+              paddingRight: '3%'
+            }}>
+            <span style={{ color: '#FF9100' }}>P</span>ERMIERE{' '}
+            <span style={{ color: '#FF9100' }}>F</span>IVERR{' '}
+            <span style={{ color: '#FF9100' }}>E</span>NVATO
+          </p>
+        </Link>
       </div>
-    );
+    )
   }
 }
 
-export default Navbar;
+export default Navbar
