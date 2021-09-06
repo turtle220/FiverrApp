2fadb57a14c1748bc3b9c369c4c526809dc602a1
Daniel <turtleluck20@gmail.com>
2021-09-05 23:57:44 -0500

    test deploy to heroku

--- server.js
+++ server.js
@@ -31,13 +31,11 @@ mongoose
 // Passport middleware
 app.use(passport.initialize());
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-// Passport config
-// require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
